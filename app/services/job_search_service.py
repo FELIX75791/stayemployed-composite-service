4ea20c019236b8ecfc9b9794f4c72f13fdf00b9f
Bonny Quan <79234220+BonnyQuan@users.noreply.github.com>
2024-10-29 22:29:01 -0400

    Update job_search_service.py
    added PUT function

--- app/services/job_search_service.py
+++ app/services/job_search_service.py
@@ -3,8 +3,6 @@ from ..util import async_get
 
 JOB_SEARCH_SERVICE_URL = os.getenv("JOB_SEARCH_SERVICE_URL")
 
-
-# TODO : replace below according to actual service
 import os
 from ..util import async_get, async_post
 
@@ -28,18 +26,6 @@ async def search_jobs_by_location(location):
     params = {"location": location}
     return await async_get(url, params=params)
 
-# Function to filter jobs by date or other criteria
-async def filter_jobs(date=None, job_type=None, experience_level=None):
-    url = f"{JOB_SEARCH_SERVICE_URL}/jobs/filter"
-    params = {}
-    if date:
-        params["date"] = date
-    if job_type:
-        params["job_type"] = job_type
-    if experience_level:
-        params["experience_level"] = experience_level
-    return await async_get(url, params=params)
-
 # Function to apply for a job
 async def apply_for_job(job_id, user_id, resume_url):
     url = f"{JOB_SEARCH_SERVICE_URL}/jobs/{job_id}/apply"
@@ -47,5 +33,15 @@ async def apply_for_job(job_id, user_id, resume_url):
         "user_id": user_id,
         "resume_url": resume_url
     }
+# PUT method to update job details
+async def update_job_details(job_id, title=None, description=None, location=None):
+    url = f"{JOB_SEARCH_SERVICE_URL}/jobs/{job_id}"
+    data = {}
+    if title:
+        data["title"] = title
+    if description:
+        data["description"] = description
+    if location:
+        data["location"] = location
     return await async_post(url, json=data)
 
